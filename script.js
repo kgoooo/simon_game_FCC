fcde345bd38221249adf4dc6ab9074bc949c6872
Kristen Goehringer <kgoehringer@gmail.com>
2017-09-05 15:09:18 +0200

    initial build, basic html & css layout.  starting to define variables

---
+++ script.js
@@ -0,0 +1,6 @@
+// GLOBAL VARIABLES
+var colorsArr = ['R', 'B' , 'Y', 'G'];  //all possible colors for Simon to pick
+var turn = 0;  //this is the turn or level counter for each step in the game
+var aiSequence = [];
+var humanSequence = [];
+var checkingCounter = 0;
