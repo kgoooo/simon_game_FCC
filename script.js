4b7842082d163a63ca9882ce23fbf402d901f5b5
Kristen Goehringer <kgoehringer@gmail.com>
2017-09-06 17:50:41 +0200

    90% functional, styled with CSS.

--- script.js
+++ script.js
@@ -4,8 +4,11 @@ var turn = 0;  //this is the turn or level counter for each step in the game
 var aiSequence = [];
 var simonColorPick;
 var humanPress;
+var simonSaysCounter = 0;
 var checkingCounter = 0;
 var gamePlaying = false;  //game status - will be affected by the on off button
+var status = 'correct';
+var strict = false;
 
 //AUDIO FILES
 var audio0 = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3');
@@ -15,15 +18,103 @@ var audio3 = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3');
 
 // initialize event listeners 
 $(document).ready(() => {
+	$('#wrong').hide();
 	$('#on').on('click', () => {
 		gamePlaying = true;
-		console.log('You\'re playing!');
+		setTimeout(simonTurn(), 500);
+		// simonTurn();
 	});
 	$('#off').on('click', () => {
 		gamePlaying = false;
 	});
 });
 
+
+// this is what happens when a play button gets clicked by user.
+var humanButtonPress = (buttonPressedIndex) => {
+	humanPress = buttonPressedIndex;
+	//check human press against ai[checkCounter index]
+	checkHumMove();	
+	checkingCounter++;
+	console.log('checking counter:' + checkingCounter);
+	// this is to make sure simon doesnt start before you have pressed all the buttons
+	if (checkingCounter == aiSequence.length && status == 'correct'){
+		checkingCounter = 0;
+		setTimeout(() => {
+			simonTurn();				
+		}, 1000);
+	} 
+	if (status === 'error'  && strict == false){
+		$('#counter').text(' --');
+		simonSaysCounter = 0;
+		checkingCounter = 0;
+		setTimeout(() => {
+			$('#counter').text(turn);		
+			simonSays();
+			status = 'correct';
+		}, 1000);
+	}
+};
+
+var strictMode = () => {
+	if(strict == false) {
+		strict = true;
+	} else{
+		strict = false;
+	}
+};
+
+var checkHumMove = () => {
+	if (aiSequence[checkingCounter] === humanPress){
+		console.log('correct');
+		status = 'correct';
+	} else {
+		console.log('WRONGGGGGG');
+		status = 'error';
+		// simonSays();
+	}
+};
+var simonSays = () => {
+	aiArrayPlayBack(aiSequence[simonSaysCounter]);
+	if (simonSaysCounter < aiSequence.length){
+		simonSaysCounter++;
+		setTimeout(simonSays, 800);
+	}
+};
+
+var aiArrayPlayBack = (playing) => {
+	switch (playing) {
+	case 0: 
+		$('#R').addClass('redHighlight');
+		audio0.play();
+		setTimeout(() => {
+			$('#R').removeClass('redHighlight');
+		}, 600);
+		break;	
+	case 1: 			
+		$('#B').addClass('blueHighlight');		
+		audio1.play();		
+		setTimeout(() => {
+			$('#B').removeClass('blueHighlight');			
+		}, 600);
+		break;
+	case 2: 
+		$('#G').addClass('greenHighlight');	
+		audio2.play();			
+		setTimeout(() => {
+			$('#G').removeClass('greenHighlight');			
+		}, 600);		
+		break;
+	case 3: 		
+		$('#Y').addClass('yellowHighlight');		
+		audio3.play();			
+		setTimeout(() => {
+			$('#Y').removeClass('yellowHighlight');				
+		}, 600);					
+	}
+};
+
+//picks the move for Simon to make
 var pickNextColorIndex = () => {
 	simonColorPick = Math.floor(Math.random()* 4);
 };
@@ -35,89 +126,9 @@ var storeColor = () => {
 
 var simonTurn = () => {
 	turn++;
-	$('#counter').text('turn:' + turn);
+	simonSaysCounter = 0;	
+	$('#counter').text(turn);
 	pickNextColorIndex();
 	storeColor();	
-	// console.log('picked index' + colorsArr[simonColorPick]);
-	// console.log(aiSequence);
-	if(turn == 1) {
-		$(`#${colorsArr[simonColorPick]}`).addClass('press');
-		//play the sound for the color
-		setTimeout(() => {
-			$(`#${colorsArr[simonColorPick]}`).removeClass('press');
-		}, 1000);
-		//need to check if turn is 1  if its 1, it does normal play begin, but when >= 2, it has to then play back the entire array with add and remove classes.
-	}
-	if (turn >= 2 ) {
-		aiArrayPlayBack();
-		$(`#${colorsArr[simonColorPick]}`).addClass('press');
-		//play the sound for the color
-		setTimeout(() => {
-			$(`#${colorsArr[simonColorPick]}`).removeClass('press');
-		}, 1000);
-		//need to check if turn is 1  if its 1, it does normal play begin, but when >= 2, it has to then play back the entire array with add and remove classes.
-	}
-	
-};
-
-simonTurn();
-
-var humanTurn = () => {
-	//turn is only as long as aiSequence.length
-	//Human button press should run inside of here? that way its sort of a constant state while human keeps pushing buttons
-	//checking counter goes ++ with every human button press
-};
-
-// this is what happens when a play button gets clicked by user.
-var humanButtonPress = (buttonPressedIndex) => {
-	humanPress = buttonPressedIndex;
-	console.log('humanPress: ' + humanPress);
-	//check human press against ai[checkCounter index]
-	checkHumMove();	
-	checkingCounter++;
-	console.log('checking counter:' + checkingCounter);
-	// this is to make sure simon doesnt start before you have pressed all the buttons
-	if (checkingCounter == aiSequence.length){
-		checkingCounter = 0;
-		setTimeout(() => {
-			simonTurn();				
-		}, 1000)
-	}
-};
-
-var checkHumMove = () => {
-	if (aiSequence[checkingCounter] === humanPress){
-		console.log('correct');
-	}
-};
-
-var aiArrayPlayBack = () => {
-	for (var i = 0; i < aiSequence.length; i++) {
-		//add the press class and remove it for each item in squence
-		//play the sound for each item in sequence.
-		switch (aiSequence[i]) {
-		case 0: 
-			audio0.play();
-			
-			setTimeout(() => {
-			}, 1000);
-			break;			
-		case 1: 
-			setTimeout(() => {
-				audio1.play();
-			}, 1000);
-			break;
-		case 2: 
-			setTimeout(() => {
-				audio2.play();
-			}, 1000);
-			break;
-		case 3: 
-			setTimeout(() => {
-				audio3.play();
-			}, 1000);
-			break;
-		}
-		// wait 1200ms before next play
-	}
+	simonSays();
 };
